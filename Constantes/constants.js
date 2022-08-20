32f2652bcaec1173ae5b4a945dd01ebbbaf4f524
= <=>
2022-08-20 14:13:20 -0500

    version constants titanium

--- Constantes/constants.js
+++ Constantes/constants.js
@@ -1,4 +1,4 @@
 const HOST = '187.190.62.113/TCN_WEB/TNC'
 
 export const API_URL_GRAL = `http://${HOST}/`;
-export const VersionSys = 'v2.1.3 Mazapán' //Muégano';
+export const VersionSys = 'v2.1.4 Mazapán' //Muégano';
